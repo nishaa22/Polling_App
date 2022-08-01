0f46af48f28e8e99a3b7d1d723a3a0d391b79bd5
nishaa22 <nisha.rani@excellencetechnologies.in>
2022-08-01 18:22:11 +0530

    edit poll fixed & dynamic form

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ import AdminDashboard from "./components/AdminDashboard";
 import ListUsers from "./components/ListUsers";
 import EditPoll from "./components/EditPoll";
 import Profile from "./components/Profile";
-import ListPoll from "./components/ListPoll";
+import ListPoll from "./components/ListPoll"
 import AddNewPollOption from "./components/AddNewPollOption";
 
 function App() {
+
   return (
     <>
       <Provider store={store}>
@@ -28,8 +29,8 @@ function App() {
             <Route path="/addnewpoll" element={<AddNewPoll />} />
             <Route path="/userlist" element={<ListUsers />} />
             <Route path="/profile" element={<Profile />} />
-            <Route exact path="/editpoll/:_id" element={<EditPoll />} />
-            <Route exact path="/listpoll/:_id" element={<ListPoll />} />
+            <Route exact path="/editpoll/:_id" element={<EditPoll/>} />
+            <Route exact path="/listpoll/:_id" element={<ListPoll/>} />
           </Routes>
         </BrowserRouter>
       </Provider>
